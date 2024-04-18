1a00cce0b3cac80406cbf362320426f93eacf7c5
aidan <aidankhogg@gmail.com>
2024-04-18 10:52:17 +0100

    WIP

---
+++ engine/reporter/logger/log_stack.py
@@ -0,0 +1,5 @@
+"""
+This module opens app-based logging into or with third-party libraries and tools such as the ELK stack.
+"""
+
+raise NotImplemented("Logging involving third-party systems is currently not supported.")
